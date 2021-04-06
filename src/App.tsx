b2697507a8c6776935892cfae2ae53979dff9830
gledson-ss <glegogle84@gmail.com>
2021-04-05 21:28:14 -0300

    :book: docs: update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-const App: React.FC = () => <h1>ola</h1>;
+const App: React.FC = () => <h1>ola 2</h1>;
 
 export default App;
