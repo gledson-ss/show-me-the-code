3feef8040e08c976a7596dbd4fcb92326a01c03b
gledson-ss <glegogle84@gmail.com>
2021-04-09 10:20:14 -0300

    :hammer: chore: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import AppProvider from './hooks/AppProvider';
 import Routes from './routes';
 import GlobalStyles from './styles/global';
 import { theme } from './styles/theme';
 
 const App: React.FC = () => (
   <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <Routes />
+    <AppProvider>
+      <GlobalStyles />
+      <Routes />
+    </AppProvider>
   </ThemeProvider>
 );
 
