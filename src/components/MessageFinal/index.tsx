805910157364f2f6ba73286d2f640b0ad82c6e31
gledson-ss <glegogle84@gmail.com>
2021-04-09 06:53:08 -0300

    :hammer: chore: add component message

---
+++ src/components/MessageFinal/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import * as St from './styles';
+
+interface props{
+  price: string;
+}
+
+const MessageFinal: React.FC<props> = ({ price }: props) => (
+  <St.Container>
+    <St.MessageTitleContainer>
+      <St.Title>
+        Obrigado pela Preferência
+      </St.Title>
+    </St.MessageTitleContainer>
+    <St.PriceContainer>
+      <St.Text>
+        Preço: R$
+        {' '}
+        {price}
+      </St.Text>
+    </St.PriceContainer>
+    <St.ButtonContainer>
+      <St.Button>Voltar ao início</St.Button>
+    </St.ButtonContainer>
+  </St.Container>
+);
+export default MessageFinal;
---
+++ src/components/MessageFinal/styles.ts
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MessageTitleContainer = styled.div`
+  width: 100%;
+  height: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h2`
+  font-family: "Noto Sans SC", sans-serif;
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+export const PriceContainer = styled.div`
+  width: 100%;
+  height: 60%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Text = styled.p`
+  font-family: "Noto Sans SC", sans-serif;
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 24px;
+`;
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  height: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Button = styled.button`
+  width: 100px;
+  height: 40px;
+  border: none;
+  :focus {
+    outline: none;
+  }
+  background-color: ${({ theme }) => theme.colors.blue};
+  color: ${({ theme }) => theme.colors.white};
+  :hover {
+    cursor: pointer;
+  }
+`;
