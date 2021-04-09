333e398d02ef31aff77c798ca3d888d38ffaf8dc
gledson-ss <glegogle84@gmail.com>
2021-04-09 06:52:53 -0300

    :hammer: chore: add method convert

--- src/components/FormBlock/index.tsx
+++ src/components/FormBlock/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import CalculateRate from '../../utils/calculateRate';
+import MessageFinal from '../MessageFinal';
 import * as St from './styles';
 
 const FormBlock: React.FC = () => {
@@ -10,11 +11,11 @@ const FormBlock: React.FC = () => {
   const [minutes, setMinutes] = useState<number>(0);
   const [hasPlan, setHasPlan] = useState<boolean>(false);
   const [hasChecked, setHasChecked] = useState<boolean[]>([false, false]);
-  const [price, setPrice] = useState<number>(0);
+  const [price, setPrice] = useState<number>(-1);
   const [hasCheckPlanValue, setHasCheckPlanValue] = useState<boolean[]>([false, false, false]);
   const [planValue, setPlanValue] = useState<number>(0);
   const [screenFinal, setScreenFinal] = useState<boolean>(false);
-
+  const [priceConv, setPriceConv] = useState<string>('');
   function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
     const { name } = e.currentTarget;
     const valueInput = e.currentTarget.value;
@@ -93,64 +94,82 @@ const FormBlock: React.FC = () => {
     priceConverter.setHasPlan(hasChecked[0]);
     priceConverter.setPlan(planValue);
 
-    setPrice(priceConverter.getPrice());
     if (Number.isNaN(price)) {
       alert('DDD de origem e destino nao cadastrado ainda!');
     }
-    setScreenFinal(true);
+    setPrice(priceConverter.getPrice());
+
+    if (price >= 0) {
+      setScreenFinal(true);
+    }
+  }
+
+  function handlePriceConv(): string {
+    const str = String(price).split('.');
+
+    if (str.length === 1) {
+      return (`${str[0]}.00`);
+    } if (str[1].length === 1) {
+      return (`${str[0]}.${str[1]}0`);
+    }
+
+    return `${price}`;
   }
 
   return (
     <St.Container>
       <St.FormBox>
-        <St.TitleInputContainer>
-          <St.TitleForm>Calcule o valor da sua chamada</St.TitleForm>
-        </St.TitleInputContainer>
-        <St.Form>
-          <St.InputContainer>
-            <St.NameInputContainer>
-              <St.NameInput>DDD Origem</St.NameInput>
-            </St.NameInputContainer>
-            <St.BoxContainer>
-              <St.Input placeholder="Digite DDD origem" name="origem" onChange={handleInput} />
-            </St.BoxContainer>
-          </St.InputContainer>
+        {
+        screenFinal ? <MessageFinal price={handlePriceConv()} /> : (
+          <>
+            <St.TitleInputContainer>
+              <St.TitleForm>Calcule o valor da sua chamada</St.TitleForm>
+            </St.TitleInputContainer>
+            <St.Form>
+              <St.InputContainer>
+                <St.NameInputContainer>
+                  <St.NameInput>DDD Origem</St.NameInput>
+                </St.NameInputContainer>
+                <St.BoxContainer>
+                  <St.Input placeholder="Digite DDD origem" name="origem" onChange={handleInput} />
+                </St.BoxContainer>
+              </St.InputContainer>
 
-          <St.InputContainer>
-            <St.NameInputContainer>
-              <St.NameInput>DDD Destino</St.NameInput>
-            </St.NameInputContainer>
-            <St.BoxContainer>
-              <St.Input placeholder="Digite DDD destino" name="destino" onChange={handleInput} />
-            </St.BoxContainer>
-          </St.InputContainer>
-          <St.InputContainer>
-            <St.NameInputContainer>
-              <St.NameInput>Minutos Falados</St.NameInput>
-            </St.NameInputContainer>
-            <St.BoxContainer>
-              <St.Input placeholder="Quantos minutos? " name="minutos" onChange={handleInput} />
-            </St.BoxContainer>
-          </St.InputContainer>
+              <St.InputContainer>
+                <St.NameInputContainer>
+                  <St.NameInput>DDD Destino</St.NameInput>
+                </St.NameInputContainer>
+                <St.BoxContainer>
+                  <St.Input placeholder="Digite DDD destino" name="destino" onChange={handleInput} />
+                </St.BoxContainer>
+              </St.InputContainer>
+              <St.InputContainer>
+                <St.NameInputContainer>
+                  <St.NameInput>Minutos Falados</St.NameInput>
+                </St.NameInputContainer>
+                <St.BoxContainer>
+                  <St.Input placeholder="Quantos minutos? " name="minutos" onChange={handleInput} />
+                </St.BoxContainer>
+              </St.InputContainer>
 
-          <St.InputContainer>
-            <St.NameInputContainer>
-              <St.NameInput>Possui Fale Mais?</St.NameInput>
-            </St.NameInputContainer>
+              <St.InputContainer>
+                <St.NameInputContainer>
+                  <St.NameInput>Possui Fale Mais?</St.NameInput>
+                </St.NameInputContainer>
 
-            <St.CheckBoxContainer>
-              <St.OptionName>Sim</St.OptionName>
-              <St.OptionContainer>
-                <St.InputCheckBox type="checkbox" name="sim" onChange={handleCheckbox} />
-              </St.OptionContainer>
-              <St.OptionName>Não</St.OptionName>
-              <St.OptionContainer>
-                <St.InputCheckBox type="checkbox" name="nao" onChange={handleCheckbox} />
-              </St.OptionContainer>
-            </St.CheckBoxContainer>
-          </St.InputContainer>
+                <St.CheckBoxContainer>
+                  <St.OptionName>Sim</St.OptionName>
+                  <St.OptionContainer>
+                    <St.InputCheckBox type="checkbox" name="sim" onChange={handleCheckbox} />
+                  </St.OptionContainer>
+                  <St.OptionName>Não</St.OptionName>
+                  <St.OptionContainer>
+                    <St.InputCheckBox type="checkbox" name="nao" onChange={handleCheckbox} />
+                  </St.OptionContainer>
+                </St.CheckBoxContainer>
+              </St.InputContainer>
 
-          {
+              {
             hasChecked[0] && (
             <St.PlansContainer>
               <St.DescriptionPlanContainer>
@@ -174,19 +193,23 @@ const FormBlock: React.FC = () => {
             )
           }
 
-          <St.SubmitContainer>
-            <St.ButtonContainer>
-              <St.ButtonReset type="reset">
-                <St.ButtonText>Reset</St.ButtonText>
-              </St.ButtonReset>
-            </St.ButtonContainer>
-            <St.ButtonContainer>
-              <St.ButtonSend type="button" onClick={handleSubmit}>
-                <St.ButtonText>Calcular</St.ButtonText>
-              </St.ButtonSend>
-            </St.ButtonContainer>
-          </St.SubmitContainer>
-        </St.Form>
+              <St.SubmitContainer>
+                <St.ButtonContainer>
+                  <St.ButtonReset type="reset">
+                    <St.ButtonText>Reset</St.ButtonText>
+                  </St.ButtonReset>
+                </St.ButtonContainer>
+                <St.ButtonContainer>
+                  <St.ButtonSend type="button" onClick={handleSubmit}>
+                    <St.ButtonText>Calcular</St.ButtonText>
+                  </St.ButtonSend>
+                </St.ButtonContainer>
+              </St.SubmitContainer>
+            </St.Form>
+          </>
+        )
+      }
+
       </St.FormBox>
     </St.Container>
   );
